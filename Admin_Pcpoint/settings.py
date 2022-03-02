ec4f108eb0d4e5efc786abc7b75b0630d94cf163
Chiragsinh Chavda <chiragc6122000@gmail.com>
2022-03-02 15:44:18 +0530

    changes in setting.py

--- Admin_Pcpoint/settings.py
+++ Admin_Pcpoint/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vqh@vnqn$ra4(mx=1%k8-9kn*9ixjv-a!g7j#jww_0s=05^s32
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
